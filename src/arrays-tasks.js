dfb2d1cdb033f6bca0a6a29a2ab4f199958b5844
drole <genuines.forever@gmail.com>
2024-02-12 01:26:00 +0400

    feat: add getStringsLength and getAverage solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -118,8 +118,8 @@ function removeFalsyValues(arr) {
  *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
  *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  return arr.map((e) => e.length);
 }
 
 /**
@@ -136,8 +136,10 @@ function getStringsLength(/* arr */) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  return arr.length
+    ? Number((arr.reduce((acc, curr) => acc + curr, 0) / arr.length).toFixed(2))
+    : 0;
 }
 
 /**
