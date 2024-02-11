086df3c65cd52d21591ccd81b481b43e13e6d1a6
drole <genuines.forever@gmail.com>
2024-02-12 02:29:34 +0400

    fix: comment variables in function to fix lint errors

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -356,7 +356,7 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(arr, chunkSize) {
+function createChunks(/* arr, chunkSize */) {
   throw new Error('Not implemented');
 }
 
